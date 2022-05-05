fb34817672272dd1ea9464db538aa30c5ff0dffb
Anh Pham <anhpt379@gmail.com>
2022-05-05 06:59:24 +0200

    `--fix` to convert camelCase to snake_case

--- lib/puppet-lint/plugins/variable_contains_upcase.rb
+++ lib/puppet-lint/plugins/variable_contains_upcase.rb
@@ -1,19 +1,24 @@
 PuppetLint.new_check(:variable_contains_upcase) do
   def check
-    tokens.select { |r|
+    tokens.select do |r|
       Set[:VARIABLE, :UNENC_VARIABLE].include? r.type
-    }.each do |token|
-      if token.value.gsub(/\[.+?\]/, '').match(/[A-Z]/)
-        notify :warning, {
-          :message => 'variable contains a capital letter',
-          :line    => token.line,
-          :column  => token.column,
-          :token   => token,
-        }
-      end
+    end.each do |token|
+      next unless token.value.gsub(/\[.+?\]/, '').match(/[A-Z]/)
+
+      notify :warning, {
+        message: 'variable contains a capital letter',
+        line: token.line,
+        column: token.column,
+        token: token
+      }
     end
   end
+
   def fix(problem)
-    problem[:token].value.downcase!
+    problem[:token].value.gsub(/::/, '/')
+                   .gsub(/([A-Z]+)([A-Z][a-z])/, '\1_\2')
+                   .gsub(/([a-z\d])([A-Z])/, '\1_\2')
+                   .tr('-', '_')
+                   .downcase
   end
 end
